f7d880fbc2aa0d1487d04ffd83ffc7b327ef7d56
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-10 07:43:15 +0600

    Update antibot.js

--- Priyansh/events/antibot.js
+++ Priyansh/events/antibot.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - TUMI BOT ER NWME CHANGE KORTE PARBA NA HEHE 😹🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - TUMI BOT ER NAME CHANGE KORTE PARBA NA HEHE 😹🖐`}, threadID);
     }  
         }
