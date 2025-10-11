0765929f0a6fd10f4fa1049312c31201524a4609
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 16:34:26 +0600

    Update cardcute.js

--- Priyansh/commands/cardcute.js
+++ Priyansh/commands/cardcute.js
@@ -26,7 +26,7 @@ module.exports.circle = async (image) => {
   return await image.getBufferAsync("image/png");
 }
 module.exports.run = async function ({ api, event, args, Users }) {
-  if ((this.config.credits) != "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭") { return api.sendMessage(`⚡️Detected credits have been changed`, event.threadID, event.messageID)}
+  if ((this.config.credits) != "≛⃝𝙰𝚈𝙴𝙰𝙷𝙰 𝚀𝚄𝙴𝙴𝙽👑") { return api.sendMessage(`⚡️Detected credits have been changed`, event.threadID, event.messageID)}
   let { senderID, threadID, messageID } = event;
   const { loadImage, createCanvas } = require("canvas");
   const request = require('request');
@@ -119,4 +119,6 @@ ctx.fillStyle = "#EEC591";
     () => fs.unlinkSync(pathImg),
     messageID
   );
-};
+};
+
+
