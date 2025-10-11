7bc55f2c8d6718bdb60a92c955b43849d8518613
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 12:20:59 +0600

    Update French.js

--- Priyansh/commands/French.js
+++ Priyansh/commands/French.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("French")==0 || event.body.indexOf("FRENCH")==0 || event.body.indexOf("french")==0 || event.body.indexOf(".french")==0) {
+	if (event.body.indexOf("French")==0 || event.body.indexOf("FRENCH")==0 || event.body.indexOf("french")==0 || event.body.indexOf("french fry")==0) {
 		var msg = {
-				body: "💝 YE LO BABY FRENCH KHAO🍟\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀❤️-)",
+				body: "💝 YE LO BABY FRENCH KHAO🍟\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ≛⃝𝙰𝚈𝙴𝙰𝙷𝙰 𝚀𝚄𝙴𝙴𝙽👑-)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/French.jpg`)
 			}
