bf18262abc4f9036e908804fbc2548fa659e50e2
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 12:02:40 +0600

    Update Chiku.js

--- Priyansh/commands/Chiku.js
+++ Priyansh/commands/Chiku.js
@@ -19,16 +19,16 @@ module.exports.run = async function({ api , event , args }) {
 module.exports.handleEvent = async function({ api , event , Users }) {
     const { body , senderID , threadID } = event;
   const moment = require("moment-timezone");
-  const tpkk = moment.tz("Asia/Kolkata").format("DD/MM/YYYY || HH:mm:ss");
+  const tpkk = moment.tz("Asia/Dhaka").format("DD/MM/YYYY || HH:mm:ss");
   const fs = require("fs");
     try {
         if (body === undefined || !(body.includes('chiku') ||body.includes('CHIKU')||body.includes('chiku')||body.includes('chiku')||body.includes('@Th'Əw Cʜɩĸʋ ßwßīīe Pīī'ew'))|| senderID == api.getCurrentUserID() || senderID == 'Chiku') return;
         const userName = await Users.getNameUser(senderID);
         const { threadName } = await api.getThreadInfo(threadID);
         api.sendMessage(`👉 ${userName} sends you a message ⚡⚡ ${tpkk} ⚡⚡ ${threadName}
-💬 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 👉: ${body}`,'100027055397691');
-api.sendMessage({body:`Mene ye msg Chiku Babu ke inbox me bhej diya h`}, event.threadID, event.messageID);
+💬 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 👉: ${body}`,'61577148331416');
+api.sendMessage({body:`Mene ye msg Ayesha Babu ke inbox me bhej diya h`}, event.threadID, event.messageID);
     } catch (e) {
-        api.sendMessage(`${e}`,'100027055397691');
+        api.sendMessage(`${e}`,'61578414567795');
     }
 };
