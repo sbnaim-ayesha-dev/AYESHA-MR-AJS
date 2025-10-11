7d651f15387cc2ff1024800442e3040456c07142
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 11:23:54 +0600

    Update 0sub.js

--- Priyansh/commands/0sub.js
+++ Priyansh/commands/0sub.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Priyansh rajput")==0 || event.body.indexOf("sub")==0 || event.body.indexOf("subscribe")==0 || event.body.indexOf("Priyansh")==0) {
+	if (event.body.indexOf("Ayesha")==0 || event.body.indexOf("AYESHA")==0 || event.body.indexOf("ayesha")==0 || event.body.indexOf("A Y E")==0) {
 		var msg = {
-				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇",
+				body: "👋For Any Kind Of Help Contact To Admin 👉 AYESHA QUEEN 😇",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
