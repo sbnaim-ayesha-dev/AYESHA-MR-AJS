0b8f27ad72b2279c934e84e08924d2f505713031
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 13:28:36 +0600

    Update Pasta.js

--- Priyansh/commands/Pasta.js
+++ Priyansh/commands/Pasta.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("PASTA")==0 || event.body.indexOf("Pasta")==0 || event.body.indexOf("pasta")==0 || event.body.indexOf(".pasta")==0) {
 		var msg = {
-				body: "💝 YE LO BBY PASTA KHAO 🥐\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀❤️-)",
+				body: "💝 YE LO BBY PASTA KHAO 🥐\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ≛⃝𝙰𝚈𝙴𝙰𝙷𝙰 𝚀𝚄𝙴𝙴𝙽👑-)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Pasta.jpg`)
 			}
