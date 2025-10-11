9c4908a49b116968d52bcd8aee857035a27b0def
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 11:00:14 +0600

    Update 0aaryan3.js

--- Priyansh/commands/0aaryan3.js
+++ Priyansh/commands/0aaryan3.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("😍")==0 || event.body.indexOf("😻")==0 || event.body.indexOf("❤️")==0 || event.body.indexOf("🤗")==0) {
 		var msg = {
-				body: "💐𝐀𝐧𝐤𝐡𝐨 𝐌𝐞 𝐏𝐲𝐚𝐫, 𝐃𝐢𝐥𝐦𝐞 𝐊𝐡𝐮𝐌𝐚𝐫, 𝐏𝐲𝐚𝐫 𝐓𝐨𝐡 𝐍𝐡𝐢 𝐤𝐚𝐫 𝐋𝐢𝐘𝐚 𝐌𝐮𝐉𝐇𝐬𝐞🫂💐",
+				body: "💐𝐀𝐧𝐤𝐡𝐨 𝐌𝐞 𝐏𝐲𝐚𝐫, 𝐃𝐢𝐥𝐦𝐞 𝐊𝐡𝐮𝐌𝐚𝐫, 𝐏𝐲𝐚𝐫 𝐓𝐨𝐡 𝐍𝐡𝐢 𝐤𝐚𝐫 𝐋𝐢𝐘𝐚",
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
