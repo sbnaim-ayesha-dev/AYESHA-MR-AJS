6c98d3e5d51d0e3bc074688cae970019fc5d1dfe
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-11 14:36:11 +0600

    Update ayaka.js

--- Priyansh/commands/ayaka.js
+++ Priyansh/commands/ayaka.js
@@ -127,7 +127,7 @@ var min = Math.floor(Math.random() * 2);
       if(money < 0) api.sendMessage("Do you need $100 to see the photo?",event.threadID,event.messageID)
           else {
    Currencies.setData(event.senderID, options = {money: money - 0})
-   var callback = () => api.sendMessage({body:`Photo Ayaka Hey 💞 ${link.length}\n-100 usd!`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
+   var callback = () => api.sendMessage({body:`Photo AJS Hey 💞 ${link.length}\n-100 usd!`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
      }
    };
