92b827e4bf0ec601561a414dd5b0bcd2c9a6ee70
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 09:22:08 +0600

    Update ##.js

--- Priyansh/commands/##.js
+++ Priyansh/commands/##.js
@@ -69,7 +69,7 @@ var callback = () => api.sendMessage({body:`➢ Admin and Bot Information
 ⁂ Bot Name: ${global.config.BOTNAME}
 ✧ Bot Admin: ${global.config.ADMINBOT}
 ♛ Bot Admin Link: https://www.facebook.com/share/17Wu9hVUR5/
-❂ Bot Prefix: ${global.config.PREFIX}
+❂ A messenger Bot : ${global.config.PREFIX}
 ✫ Bot Owner: https://www.facebook.com/share/17Wu9hVUR5/
 ➟ UPTIME
 ✬ Today is: ${juswa} 
