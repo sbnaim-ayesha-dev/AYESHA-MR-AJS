712dbdcb3154f017e89496c03a72efe62e153590
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 13:38:14 +0600

    Update gali_reply.js

--- Priyansh/commands/gali_reply.js
+++ Priyansh/commands/gali_reply.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bsdk")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("Bsdke")==0 || event.body.indexOf("gandu")==0) {
+	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bolod")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("abal")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("Abal")==0 || event.body.indexOf("Bolod")==0) {
 		var msg = {
-				body: "(Boss Dk , Gali q Dete Ho. Lund Katke Hath M rakh Dungi)",
+				body: "(Mem Dekho , Gali Derahe He. Lund Katke Hath M rakh Dungi)",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
