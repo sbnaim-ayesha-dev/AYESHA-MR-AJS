47f5f143d5c00afdb878a0e3ec9bfedcb322c867
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 14:40:21 +0600

    Update mention.js

--- Priyansh/commands/mention.js
+++ Priyansh/commands/mention.js
@@ -9,11 +9,11 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100027055397691") {
-    var aid = ["100027055397691"];
+  if (event.senderID !== "61577148331416") {
+    var aid = ["61577148331416"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Abee Mere se baat Kar ..Chuza Boss Ko mention kyu kar rhe ho😒" , "Kya Hua Chuza Boss ko q Bula Rhe Ho?" , "Wo Shayad Busy h😐🙄" , "Tujhe sunai nhi deta mere admin ko tang na kr😒😒" , "Mere Chuza boss ko tang naa karo" , "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me smjh nahi ata" , "Kya kaam hai mujhe bol" , "Ek Baar me samjh ni ata kyu mention kar rhe ho Chuza Boss Ko" ];
+      var msg = ["Abee Mere se baat Kar ..Ayesha Bbby Ko mention kyu kar rhe ho😒" , "Kya Hua Ayesha bby ko q Bula Rhe Ho?" , "Wo Shayad Busy h😐🙄" , "Tujhe sunai nhi deta mere admin ko tang na kr😒😒" , "Mere Ayesha BBY ko tang naa karo" , "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me smjh nahi ata" , "Kya kaam hai mujhe bol" , "Ek Baar me samjh ni ata kyu mention kar rhe ho Ayesha Bby Ko" ];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
