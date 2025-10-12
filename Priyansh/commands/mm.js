6f6d9be112800c0dd919a3af9426de64ef660f6c
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 14:49:40 +0600

    Update mm.js

--- Priyansh/commands/mm.js
+++ Priyansh/commands/mm.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("xxxx")==0 || event.body.indexOf("x")==0 || event.body.indexOf("xx")==0 || event.body.indexOf("xxx")==0) {
+	if (event.body.indexOf("xxxx")==0 || event.body.indexOf("xnx")==0 || event.body.indexOf("xx")==0 || event.body.indexOf("xxx")==0) {
 		var msg = {
 				body: "🥵 ahh Fuck 🥵",
 				attachment: fs.createReadStream(__dirname + `/noprefix/xf.mp3`)
