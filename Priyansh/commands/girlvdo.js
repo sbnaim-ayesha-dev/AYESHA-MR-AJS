10e011d9f77ed858fad9311d58caab4666487265
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 13:46:18 +0600

    Update girlvdo.js

--- Priyansh/commands/girlvdo.js
+++ Priyansh/commands/girlvdo.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["-𝗔𝗔𝗥𝗬𝗔𝗡-𝗦𝗧𝗔𝗧𝗨𝗦-"];
+   var hi = ["-AYESHA-STATUS-"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
