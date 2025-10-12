cd894c4b5e6f37397086e60d62a4c1292d63d4bb
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 16:24:59 +0600

    Update nudegirl.js

--- Priyansh/commands/nudegirl.js
+++ Priyansh/commands/nudegirl.js
@@ -53,6 +53,6 @@ module.exports.run = async ({ api, event, args, client, Users, Threads, __GLOBAL
     "https://imgur.com/19M5A6q.jpg",
     "https://i.imgur.com/FK16e5v.jpg",
   ];
-  var callback = () => api.sendMessage({ body: `All day I know Buscu Buslon. 😏`, attachment: fs.createReadStream(__dirname + "/cache/5.jpg") }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));
+  var callback = () => api.sendMessage({ body: `uhu i know that what your mind now 😏`, attachment: fs.createReadStream(__dirname + "/cache/5.jpg") }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));
   return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/5.jpg")).on("close", () => callback());
 };
