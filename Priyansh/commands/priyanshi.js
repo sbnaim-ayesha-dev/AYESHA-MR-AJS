f1ad489ec64d34eb12762cd69687e4c3cbab8d71
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 16:43:09 +0600

    Update priyanshi.js

--- Priyansh/commands/priyanshi.js
+++ Priyansh/commands/priyanshi.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   if (event.body.indexOf("@Сцтє Јдди")==0 || event.body.indexOf("Priyanshi")==0 || event.body.indexOf("priyanshi")==0 || event.body.indexOf("@Ряїүдиѕнї Мєняд")==0) {
     var msg = {
         body: "❤️𝐘𝐄 𝐋𝐎 𝐏𝐑𝐈𝐘𝐀𝐍𝐒𝐇𝐈 𝐉𝐈 𝐀𝐀 𝐆𝐀𝐈🙈",
-        attachment: fs.createReadStream(__dirname + `/cache/priya.jpg`)
+        attachment: fs.createReadStream(__dirname + `/cache/---.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💚", event.messageID, (err) => {}, true)
