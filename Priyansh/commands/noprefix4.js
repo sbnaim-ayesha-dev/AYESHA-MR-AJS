bb81f437b0ca60ad7767379a88fee44b0e57a510
naimhack999 <107484926+naimhack999@users.noreply.github.com>
2025-10-12 16:15:55 +0600

    Update noprefix4.js

--- Priyansh/commands/noprefix4.js
+++ Priyansh/commands/noprefix4.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("India")==0 || event.body.indexOf("Hind")==0 || event.body.indexOf("Bharat")==0 || event.body.indexOf("desh")==0) {
+	if (event.body.indexOf("Inddfgsdgia")==0 || event.body.indexOf("Hisdgsgnd")==0 || event.body.indexOf("Bhsgdsgarat")==0 || event.body.indexOf("dessdgsgh")==0) {
 		var msg = {
 				body: "Mera Desh Bharat🇮🇳 Jai hind 🇮🇳🇮🇳 jai bhaarat❤❤❤🇮🇳",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sad/india.mp4`)
